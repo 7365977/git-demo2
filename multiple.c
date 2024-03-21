c0544758c41aaf724465c6bd150d5f7d5264e88e
Yukin <Yukin@qq.com>
2024-03-21 08:29:41 +0800

    2.0

--- multiple.c
+++ multiple.c
@@ -4,7 +4,7 @@ int main ()
    int i, j;
     printf("输出简化九九乘法表：\n");
     for (i=1;i<=9;i++)
-    { for (j=1;j<=i;j++)
+    { for (j=1;j<=9;j++)
           printf ("%d*%d=%-4d",j,i,i*j);
       printf ("\n");
     } 
